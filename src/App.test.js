38571be6c996182d1c2d69c7fc7930b25b0d2175
quangChe <quang.n.che@gmail.com>
2019-05-30 17:11:47 -0700

    Added initial snapshot test for App component

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,10 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { shallow } from 'enzyme';
+import setupTest from './setupTests';
 import App from './App';
 
+const app = shallow(<App />);
+
 it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+  expect(app).toMatchSnapshot();
 });
