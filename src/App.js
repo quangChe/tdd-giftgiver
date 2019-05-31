e77cdfc6d6e062dc71127aebf31d37fce06809fd
quangChe <quang.n.che@gmail.com>
2019-05-30 23:44:15 -0700

    Modified App component, cleaned up snapshot test for App, ran test and everything is working as expected

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <h1>Hello world!</h1>
+    <h1>Hello World!</h1>
   );
 }
 
--- src/App.test.js
+++ src/App.test.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-import setupTest from './setupTests';
 import App from './App';
 
 const app = shallow(<App />);
