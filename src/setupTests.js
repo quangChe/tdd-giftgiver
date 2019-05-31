aff58732d7b6a21aa52bb259e42215ecfc594a38
quangChe <quang.n.che@gmail.com>
2019-05-30 17:11:13 -0700

    Export enzyme configuration from setupTest

--- src/setupTests.js
+++ src/setupTests.js
@@ -2,4 +2,4 @@ import requestAnimationFrame from './tempPolyfills';
 import { configure } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 
-configure({adapter: new Adapter(), disableLifecycleMethods: true});
+export default configure({adapter: new Adapter(), disableLifecycleMethods: true});
