abd727f5035e4ba0897a8e07187419d93f91963c
quangChe <quang.n.che@gmail.com>
2019-06-03 12:45:22 -0700

    Completed App component's functionality to pass 'add gift' button test

--- src/App.js
+++ src/App.js
@@ -1,15 +1,24 @@
 import React from 'react';
 import './App.scss';
+import { Button } from 'react-bootstrap';
 
 class App extends React.Component {
   state = {
     gifts: [],
   }
 
+  addGift = (e) => {
+    const { gifts }= this.state;
+    const ids = gifts.map(gift => gift.id);
+    const newId = ids.length ? Math.max(...ids) : 0;
+    return this.setState({gifts: gifts.concat({id: newId + 1})});
+  }
+
   render() { 
     return (
       <div>
         <h2>Gift Giver</h2>
+        <Button className="btn-add" onClick={this.addGift}>Add Gift</Button>
       </div>
     );
   }
