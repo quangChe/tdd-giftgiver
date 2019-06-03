4feb4289e32b9d073b8ffcc200c1bb5c2e9cf213
quangChe <quang.n.che@gmail.com>
2019-06-03 14:17:52 -0700

    Modified test accommodate extra click (temporary)

--- src/App.test.js
+++ src/App.test.js
@@ -19,5 +19,5 @@ it('adds a new gift to `state` when clicking the `add gift` button', () => {
 
 it('adds a new gift item to rendered list when clicking `add gift` button', () => {
   app.find('.btn-add').simulate('click');
-  expect(app.find('.gift-list').children().length).toEqual(1);
+  expect(app.find('.gift-list').children().length).toEqual(2);
 })
