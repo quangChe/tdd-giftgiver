3c8c489cfdd56a3f284a4d4071de6d388ce3d809
quangChe <quang.n.che@gmail.com>
2019-06-03 14:47:17 -0700

    Refactored App component's test to achieve a more BDD architecture

--- src/App.test.js
+++ src/App.test.js
@@ -2,22 +2,24 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import App from './App';
 
-const app = shallow(<App />);
-
-it('renders without crashing', () => {
-  expect(app).toMatchSnapshot();
-});
-
-it('initializes the state with an empty list of gifts', () => {
-  expect(app.state().gifts).toEqual([]);
-})
-
-it('adds a new gift to `state` when clicking the `add gift` button', () => {
-  app.find('.btn-add').simulate('click');
-  expect(app.state().gifts).toEqual([{id: 1}]);
-})
-
-it('adds a new gift item to rendered list when clicking `add gift` button', () => {
-  app.find('.btn-add').simulate('click');
-  expect(app.find('.gift-list').children().length).toEqual(2);
+describe('App', () => {
+  const app = shallow(<App />);
+  
+  it('renders without crashing', () => {
+    expect(app).toMatchSnapshot();
+  });
+  
+  it('initializes the state with an empty list of gifts', () => {
+    expect(app.state().gifts).toEqual([]);
+  })
+  
+  it('adds a new gift to `state` when clicking the `add gift` button', () => {
+    app.find('.btn-add').simulate('click');
+    expect(app.state().gifts).toEqual([{id: 1}]);
+  })
+  
+  it('adds a new gift item to rendered list when clicking `add gift` button', () => {
+    app.find('.btn-add').simulate('click');
+    expect(app.find('.gift-list').children().length).toEqual(2);
+  })
 })
